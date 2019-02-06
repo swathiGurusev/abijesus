a40a75199da1ece17576978b79277e8123aa4230
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-02-06 14:11:47 +0530

    Update Alphabet.java

--- Alphabet.java
+++ Alphabet.java
@@ -9,8 +9,8 @@ import java.util.Scanner;
         char c = '*';
 
         if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-            System.out.println(c + " alphabet.");
+            System.out.println( " alphabet.");
         else
-            System.out.println(c + " No.");
+            System.out.println( " No.");
     }
 }

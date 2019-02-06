c98a44aae9fe08c775e8fcb1cfe00e18213c38d3
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-02-06 14:09:25 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+ class Alphabet 
+ {
+
+    public static void main(String[] args) {
+
+        char c = '*';
+
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println(c + " alphabet.");
+        else
+            System.out.println(c + " No.");
+    }
+}

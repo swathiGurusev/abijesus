24821ebea5f0d66779490c332f101ed3b8400b4f
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-02-06 15:10:09 +0530

    Update largest.java

--- largest.java
+++ largest.java
@@ -8,16 +8,16 @@ import java.util.Scanner;
     {
         int num1, num2, num3, result, temp;
         Scanner scanner = new Scanner(System.in);
-        System.out.println(" ");
+      
         num1 = scanner.nextInt();
-        System.out.println(" ");
+       
         num2 = scanner.nextInt();
                 
-        System.out.println(" ");
+        
         num3 = scanner.nextInt();
         scanner.close();
         temp = num1>num2 ? num1:num2;
         result = num3>temp ? num3:temp;
-        System.out.println(" "+result);
+        System.out.println( result);
     }
 }

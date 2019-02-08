8787b25b244e346eaf8610fc46f4e1dd54d0a84f
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-02-08 16:32:46 +0530

    Create integer.java

---
+++ integer.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class NumberDigits {
+
+    public static void main(String[] args) {
+
+        int count = 0, num = 548;
+
+        while(num != 0)
+        {
+            // num = num/10
+            num /= 10;
+            ++count;
+        }
+
+        System.out.println("Number of digits: " + count);
+    }
+}

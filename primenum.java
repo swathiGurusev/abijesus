212ff317fc5e60d9b4059086c9343aec8867f19d
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-02-09 15:50:12 +0530

    Create primenum.java

---
+++ primenum.java
@@ -0,0 +1,24 @@
+import java.util,scanner;
+ class Prime 
+{
+
+    public static void main(String[] args) {
+
+        int num = 29;
+        boolean flag = false;
+        for(int i = 2; i <= num/2; ++i)
+        {
+            // condition for nonprime number
+            if(num % i == 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+    }
+}

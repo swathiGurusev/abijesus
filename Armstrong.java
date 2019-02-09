379828989be32c763d4750e96227547a16d7dadc
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-02-09 16:47:38 +0530

    Create Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Armstrong
+{
+
+     static void main(String[] args) {
+
+        int num = 371, number, temp, total = 0;
+
+        number = num;
+        while (number != 0)
+        {
+            temp = number % 10;
+            total = total + temp*temp*temp;
+            number /= 10;
+        }
+
+        if(total == num)
+            System.out.println( "yes");
+        else
+            System.out.println("no");
+    }
+}

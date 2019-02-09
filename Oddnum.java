ff11b3a3be1d8db2d54cca01757973a2d5418805
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-02-09 16:33:36 +0530

    Create Oddnum.java

---
+++ Oddnum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class OddNumber 
+{
+	public static void main(String args[])
+  {
+			for (int i = 1; i <= 100; i++)
+      {
+			if (i % 2 != 0)
+      {
+				System.out.print(i + " ");
+			}
+		}
+	}
+}

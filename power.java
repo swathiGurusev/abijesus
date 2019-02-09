963fdef7db79e4ad427d9364d123d29293a6b152
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-02-09 09:47:36 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,25 @@
+import java.util.*; 
+import java.lang.*; 
+import java.io.*; 
+import java.util.Scanner;
+ 
+
+ public class Power
+ {
+
+    public static void main(String[] args) 
+    {
+
+        int base = 5, exponent = 2;
+
+        long result = 1;
+
+        while (exponent != 0)
+        {
+            result *= base;
+            --exponent;
+        }
+
+        System.out.println("Answer = " + result);
+    }
+}

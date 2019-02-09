b6641a7a9b93af568115ad41b4832b38cff6ec4d
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-02-09 16:17:08 +0530

    Create evennum.java

---
+++ evennum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class ListEvenNumbers {
+ 
+	public static void main(String[] args)
+	{
+		
+		//define limit
+		int limit = 50;
+		
+		
+		for(int i=1; i <= limit; i++)
+		{
+			
+			// if the number is divisible by 2 then it is even
+			if( i % 2 == 0)
+			{
+				System.out.print(i + " ");
+			}
+		}
+	}
+}

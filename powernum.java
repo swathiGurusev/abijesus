6eb0a44b4ce07ace09933c154f3eb7dc4787ec3d
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-08 16:18:50 +0530

    Create powernum.java

---
+++ powernum.java
@@ -0,0 +1,34 @@
+import java.io.*; 
+ import java.util.*;
+class poweroftwonum
+{ 
+    static int check(int n) 
+    { 
+        if (n == 0) 
+        {
+            return 0; 
+        } 
+        else
+        {
+        while (n != 1) 
+        { 
+            if (n % 2 != 0){ return 0;} 
+            n = n / 2; 
+        } 
+        return 1;
+        } 
+    }   
+    public static void main(String args[]) 
+    { 
+        Scanner input =new Scanner(System.in);
+        int number=input.nextInt();
+        if (check(number)==1)
+        { 
+            System.out.println("yes"); 
+        }
+        else
+        {
+            System.out.println("no"); 
+        }  
+    } 
+} 

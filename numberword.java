cc09faa64e5360e245dd0d1f2fe1f5ee6b68d6b8
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-08 16:25:47 +0530

    Create numberword.java

---
+++ numberword.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Numwordvalue 
+{
+    public static void main(String args[]){
+        int m;
+        Scanner s = new Scanner(System.in);
+        m = s.nextInt();
+        switch(m)
+        {
+         case 1: System.out.println("One");
+                 break;
+         case 2: System.out.println("Two");
+                 break;
+         case 3: System.out.println("Three");
+                 break;
+         case 4: System.out.println("Four");
+                 break;
+         case 5: System.out.println("Five");
+                 break;
+         case 6: System.out.println("Six");
+                 break;
+         case 7: System.out.println("Seven");
+                 break;
+         case 8: System.out.println("Eight");
+                 break;
+         case 9: System.out.println("Nine");
+                 break;
+         case 10: System.out.println("Ten");
+                 break;                 
+        }
+      
+    }
+}

736586a9b7ca99d4460c9f097cd412ff5eafe9e3
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-08 16:11:07 +0530

    Create countdigit.java

---
+++ countdigit.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class countdigivalue
+{
+	public static void main(String args[])
+	{
+		Scanner SC=new Scanner(System.in);
+		int num=SC.nextInt();
+        int flag=0;
+        while(num!=0){num/=10;
+                flag++;}
+            System.out.print(flag);
+	}
+}

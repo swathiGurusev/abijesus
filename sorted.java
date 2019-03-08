2cc2ce37b5a32ac7b5f1be65d55416a7d55056e9
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-08 10:20:04 +0530

    Create sorted.java

---
+++ sorted.java
@@ -0,0 +1,14 @@
+import java.util.Arrays; 
+  
+ class Sortedarray 
+{ 
+    public static void main(String[] args) 
+    { 
+        // Our arr contains 8 elements 
+        int[] arr = {13, 7, 6, 45, 21, 9, 101, 102}; 
+  
+        Arrays.sort(arr); 
+  
+        System.out.printf( Arrays.toString(arr)); 
+    } 
+} 

aa0899b088f4361ebd5c97ee6a358c6b6f6c1174
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-08 16:27:49 +0530

    Create countnum.java

---
+++ countnum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+ class digitcount2 
+{
+    public static void main(String args[]){
+        int m, n,number=0;
+        Scanner s = new Scanner(System.in);
+        m = s.nextInt();
+        while(m > 0){
+            n = m % 10;
+            m = m / 10;
+             number+=n;           
+        }    
+            System.out.print(number);
+        
+    }
+}

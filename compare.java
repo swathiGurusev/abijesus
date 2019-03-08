c613cfdccb0d7a512013aa4de1753e283132077c
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-08 16:03:55 +0530

    Create compare.java

---
+++ compare.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class compare
+{
+public static void main(String args[])
+{
+Scanner SC=new Scanner(System.in);
+String str1=SC.next().toLowerCase();
+String str2=SC.next().toLowerCase();
+int len1=str1.length();
+int len2=str2.length();
+int flg=0;
+if(len1!=len2)
+{
+if(len1 >len2)
+{
+System.out.print(str1);
+}
+else
+{
+System.out.print(str2);
+}
+}
+else
+{
+for(int i=0; i<str1.length();i++)
+{
+if(str1.charAt(i)!=str2.charAt(i))
+{
+flg=1;
+break;
+}
+}
+if(flg==1)
+{
+ System.out.println(str1);
+}
+}
+}
+}

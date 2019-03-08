6a739461960f27a370f0025ed783a73885d72150
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-08 16:12:59 +0530

    Create nextno.java

---
+++ nextno.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class nextvalue
+{
+	public static void main(String args[])
+	{
+		Scanner SC=new Scanner(System.in);
+		int num=SC.nextInt();        
+            System.out.print(num+1);
+	}
+}

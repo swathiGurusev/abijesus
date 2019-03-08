8ecca42062b9fec56587dc1a7a71bf16b8ad8358
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-08 12:03:09 +0530

    Create number.java

---
+++ number.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class number{ 
+public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    String str= sc.nextLine();
+     String array[]= str.split(" ",0);
+    System.out.println(array.length);
+  }
+}

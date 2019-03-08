51940d5f9ec14d80d6d0a415ad9475db04aad425
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-08 16:31:53 +0530

    Rename evennum.java to evennumber.java

---
+++ evennumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class evennumber
+{
+public static void main(String args[])
+{
+int m;
+Scanner s = new Scanner(System.in);
+m = s.nextInt();
+if(m%2==0)
+{
+System.out.print(m);
+}
+else
+{
+System.out.print(m-1);
+}
+}
+}

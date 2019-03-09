bdb02de89c9df836200c58b27a47ce42f6eea66d
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 09:34:54 +0530

    Create alphanum.java

---
+++ alphanum.java
@@ -0,0 +1,31 @@
+import java.lang.*;
+import java.util.Scanner;
+class alphanum
+{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);  
+String str= sc.nextLine();
+count(str);}
+public static void count(String x){
+        int num1=0,num2=0;
+		char[] ch = x.toCharArray();
+        for(int i=0;i<x.length();i++)
+        {
+         if(Character.isDigit(ch[i]))
+         {
+          num1=1;
+         }
+        if(Character.isLetter(ch[i]))
+        {
+        	num2=1;
+        }
+        }
+        if(num1==1&&num2==1)
+        {
+            System.out.println("Yes");
+        }
+        else{
+              System.out.println("No");
+        }
+}
+}

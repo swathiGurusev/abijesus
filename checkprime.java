283b8560abb61f9fb84286611554a0d0fbdeb721
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 13:20:31 +0530

    Create checkprime.java

---
+++ checkprime.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class checkprime
+{
+    public static void main(String arg[]){
+        Scanner ip=new Scanner(System.in);
+        int start=ip.nextInt();
+        int end=ip.nextInt();
+        int count=0;
+        boolean prime[]=new boolean[end+1];
+        for(int i=0;i<=end;i++)
+        {
+            prime[i]=true;
+        }
+        for(int p=2;p*p<=end;p++)
+        {
+            if(prime[p]==true)
+            {
+                for(int i=p*p;i<=end;i=i+p)
+                {
+                    prime[i]=false;
+                }
+            }
+        } 
+        for(int j=2;j<=end;j++)
+        {
+            if(prime[j]==true&& j>=start)
+            { 
+                count++;
+            }
+        }
+        
+        System.out.print(count);
+    }
+}

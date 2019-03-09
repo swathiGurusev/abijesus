a47cf6506e243f2729bf40e1287b204d3fc9f59b
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 13:54:03 +0530

    Rename checkstring.java to checkabistring.java

---
+++ checkabistring.java
@@ -0,0 +1,27 @@
+import java.util.*;
+  class checkstring
+  { 
+    public static void main(String[] args) { 
+	Scanner input=new Scanner(System.in);
+        	String str = input.nextLine();
+        	int count[] = new int[256]; 
+        
+       	 int len = str.length(); 
+        	for (int i=0; i<len; i++) 
+	{	
+            		count[str.charAt(i)]++; 
+       	}
+        	int max = -1;   
+        	char result = ' ';    
+        
+        	for (int i = 0; i < len; i++) 
+	{ 
+            		if (max < count[str.charAt(i)]) 
+		{ 
+                			max = count[str.charAt(i)]; 
+                			result = str.charAt(i); 
+            		}	 
+        	}
+        	System.out.println(result); 
+    } 
+} 

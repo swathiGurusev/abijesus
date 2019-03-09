5fb049f0fb0b076e099ec7f7a77ddebdd60e7cec
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 13:59:48 +0530

    Rename abifind.java to abirakheshfind.java

---
+++ abirakheshfind.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class findnumbervalue 
+{ 
+    static int number(int arr[], int n) 
+    { 
+        for (int i = 0; i < n; i++) 
+        { 
+            int j; 
+            for (j = 0; j < n; j++) 
+                if (i != j && arr[i] == arr[j]) 
+                    break; 
+            if (j == n) 
+                return arr[i]; 
+        } 
+          
+        return -1; 
+    } 
+    public static void main (String[] args){ 
+      Scanner ip=new Scanner(System.in);
+      int n=ip.nextInt();    
+        int arr[] =new int[n];
+       for(int i=0;i<n;i++)
+       {
+           arr[i]=ip.nextInt();
+       }
+          
+        System.out.print(number(arr, n)); 
+    } 
+} 

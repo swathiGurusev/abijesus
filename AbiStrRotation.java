1bd410c842eac153874711898458b6c9afc0930c
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 15:54:34 +0530

    Create AbiStrRotation.java

---
+++ AbiStrRotation.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+public class AbiStrRotation 
+{
+    public static String StringRotation(String s,int k)
+    {
+        char ch[]=s.toCharArray();
+        char temp=' ';
+        while(k!=0)
+        {
+            temp=ch[s.length()-1];
+
+        for(int i=s.length()-2;i>=0;i--)
+        {
+            ch[i+1]=ch[i];
+        }
+            ch[0]=temp;
+            k--;
+        }
+        String a=new String(ch);
+        return a;
+    }
+    public static void main(String[] args)
+    {
+        Scanner in=new Scanner(System.in);
+        String S=in.next();
+        int K=in.nextInt();
+        String s=StringRotation(S,K);
+        System.out.println(s);
+    }
+}

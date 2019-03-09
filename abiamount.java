6a559c3bc5335f6c857dda1c66235e49ef734fd0
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 11:39:29 +0530

    Create abiamount.java

---
+++ abiamount.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class amountvalue
+{	
+	public static void main(String[] args){
+		int p,r,t;
+        Scanner	sc = new Scanner(System.in);
+        p=sc.nextInt();
+        t=sc.nextInt();
+        r=sc.nextInt();
+        System.out.print((r*t*p)/100);
+       
+	}
+}

5b769c0e0ce5ca37834697601f0cb08ae10def0c
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 13:18:04 +0530

    Create testcase.java

---
+++ testcase.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class testCase
+{
+    public static void main(String ar[]){
+        Scanner in=new Scanner(System.in);
+        String sen=in.nextLine().toLowerCase();
+        int len=0,i=0;
+        //System.out.print(sen);
+        String str[] = sen.split(" ",-2);
+        
+      ///System.out.print(str.length);        
+        while(i<str.length)
+        { 
+          int j=0;
+            char ch[]=str[i].toCharArray();
+            int chlen=ch.length;
+            while(j<chlen)
+            {
+              ch[0]=Character.toUpperCase(str[i].charAt(0));
+              System.out.print(ch[j]);
+              j++;
+            }
+            System.out.print(" ");
+          //  System.out.print(chlen);              
+        i++;
+        }
+    }
+}

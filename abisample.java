7be988763a6f73b16b8e5db9b7d9caec697398ef
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 14:45:42 +0530

    Create abisample.java

---
+++ abisample.java
@@ -0,0 +1,30 @@
+import java.util.*;
+ class abisample
+{
+public static void main(String[] args)
+{
+String s;
+int count=0;count1=0
+Scanner w=new Scanner(System.in);
+s=w.nextLine();
+for(int i=0;i<s.length();i++)
+{
+if(str.charAt(i)=='(')
+{
+count=count+1;
+}
+if(str.charAt(i)==')')
+{
+count1=count1+1;
+}
+}
+if(count==count1)
+{
+System.out.println("yes");
+}
+else
+{
+System.out.println("no");
+}
+}
+}

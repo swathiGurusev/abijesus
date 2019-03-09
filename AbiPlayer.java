6611a84f47dc2af729cfb68bb6e3940b106d7e58
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 16:06:47 +0530

    Create AbiPlayer.java

---
+++ AbiPlayer.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class AbiPlayer 
+{
+
+	public static void main(String[] args) 
+  {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+float s;
+s=(float) (a*(3.14/180));
+System.out.println(s);
+	}
+
+}

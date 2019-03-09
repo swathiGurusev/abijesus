25e44a14e136adbbee9ed000a113ee968c4ca9fb
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 13:48:26 +0530

    Create checksquare.java

---
+++ checksquare.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class checksquareofdigit
+{
+    public static void main(String ar[]){
+        Scanner input=new Scanner(System.in);
+        int num=input.nextInt();
+        int n,square=0;
+        while(num>0)
+        {
+            n=num%10;
+            square =(n*n)+square;
+            num=num/10;
+        }    
+        System.out.print(square);
+    }
+}

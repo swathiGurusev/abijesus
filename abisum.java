345d43b60f4b9ffa5758cdbf08339db6582ef9eb
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 11:30:08 +0530

    Create abisum.java

---
+++ abisum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class sumvalue
+{	
+	public static void main(String[] args){
+		int l,sum=0;
+        Scanner	sc = new Scanner(System.in);
+        l=sc.nextInt();
+        int arr[]= new int[l];
+        for(int i=0;i<l;i++)
+        {
+            arr[i]=sc.nextInt();
+            sum+=arr[i];
+        }
+        System.out.print(sum);
+       
+	}
+}

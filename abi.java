8a38cea80984d7715b4b02610e3343d80e3d3b48
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 09:57:40 +0530

    Update abi.java

--- abi.java
+++ abi.java
@@ -3,12 +3,14 @@ import java.util.*;
 public class Main 
 {
     
-public static void main(String[] args) {
+public static void main(String[] args) 
+{
     
     Scanner sc=new Scanner(System.in);
     String str=new String();
     str=sc.nextLine();
-    if(System.out.printf("%s",str) != null){
+    if(System.out.printf("%s",str) != null)
+    {
         
     } 
     

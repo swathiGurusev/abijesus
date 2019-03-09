0990554855dc4486cfec28f5ee373f586c229613
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 12:00:31 +0530

    Create strjoin.java

---
+++ strjoin.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+
+class strjoin
+{
+public static void main(String ag[])
+{
+Scanner sc=new Scanner(System.in);
+String str1=new String();
+String str2=new String();
+str1=sc.nextLine();
+str2=sc.nextLine();
+
+System.out.println(str1+str2);
+}
+}

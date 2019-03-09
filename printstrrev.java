c19167c1e567b84b5e34d4ba7d0657320b84df75
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 12:18:06 +0530

    Create printstrrev.java

---
+++ printstrrev.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+ class printstrrev
+ {
+    public static void main(String args[])
+    {   
+        int fact=0;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        while(n!=0)
+        {
+            int digit=n%10;
+            fact=fact*10+digit;
+            n/=10;
+        }
+        System.out.println(fact);
+    }
+}

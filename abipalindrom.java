0c37c649c71de3c54501fa0e93e1e1c33f9640c2
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 10:11:29 +0530

    Create abipalindrom.java

---
+++ abipalindrom.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class palin
+{
+public static void main(String args[])
+{
+String original, reverse = "";
+Scanner in = new Scanner(System.in);
+original = in.next();
+int length = original.length();
+for (int i = length - 1; i >= 0; i--)
+{
+reverse += original.charAt(i);
+}  
+if (original.equals(reverse))
+{
+System.out.println("yes");
+}
+else
+{
+System.out.println("no");
+} 
+}
+} 

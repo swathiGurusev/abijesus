cc31bbda6d574783ae691920ed98ec0cda7b9800
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 11:33:40 +0530

    Create abicloser.java

---
+++ abicloser.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class findsumcloser
+{	
+	public static void main(String[] args){
+		int l,find;
+        Scanner	sc = new Scanner(System.in);
+        l=sc.nextInt();
+        find=sc.nextInt();
+        int arr[]= new int[l];
+        for(int i=0;i<l;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.print(arr[find-1]);
+       
+	}
+}

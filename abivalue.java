fb813674e778c6a1f9b67894037130166d9a2e87
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 11:52:25 +0530

    Create abivalue.java

---
+++ abivalue.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class prodectvalue
+{
+    public static void main(String args[])
+    {  
+        String str;
+        Scanner s = new Scanner(System.in);
+        str = s.next();
+        int n=s.nextInt();
+        int len=str.length();
+        int find=len-n;
+        System.out.println(str.substring(find,len));
+    }
+}

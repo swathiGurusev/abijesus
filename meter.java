9bd1f98712ada5cbd1173528610630c014002919
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 11:22:58 +0530

    Create meter.java

---
+++ meter.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class 1cmetter
+{	
+	public static void main(String[] args){
+		int Num1, Num2, GCD = 0, LCM = 0;
+        Scanner	sc = new Scanner(System.in);
+	    int a = Num1 = sc.nextInt();	
+        int b = Num2 = sc.nextInt();
+		while(Num2 != 0)
+	    {
+			if(Num1 > Num2)
+			{
+				Num1 = Num1 - Num2;
+			}
+			else
+			{
+				Num2 = Num2 - Num1;
+			}
+	    }
+		GCD = Num1;
+		LCM = (a * b) / GCD;
+		System.out.println(LCM);
+	}
+}

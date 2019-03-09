ff83508c9bbc6544c5061ae237e9135555bc47df
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 14:19:44 +0530

    Create abirakheshnumaric.java

---
+++ abirakheshnumaric.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+class abirakheshnumaric
+{
+    public static void main(String ar[]){
+        Scanner input=new Scanner(System.in);
+        String str=input.next();
+        int len=str.length();
+        int count=0;
+        for(int i=0;i<len;i++)
+        {
+            if(Character.isDigit(str.charAt(i)))
+            {
+                count++;
+            }
+        }
+        if(count==len)
+        {
+            System.out.print("yes");
+        }
+        else
+        {
+            System.out.print("no");
+        }    
+    }
+}

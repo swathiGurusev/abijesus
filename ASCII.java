96cf3f1aef28b410d27f7c2a3664651899f8da9d
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 11:07:41 +0530

    Create ASCII.java

---
+++ ASCII.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class asciivaluedigits 
+{   
+    public static void main(String[] args){ 
+        Scanner input=new Scanner(System.in);
+        char c =input.next().charAt(0); 
+        int ascii = c; 
+        System.out.println(ascii); 
+    } 
+} 

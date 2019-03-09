42a77826a34e378016d845723c24ccc6c3de7748
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 09:43:06 +0530

    Create numofdigit.java

---
+++ numofdigit.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Digits
+{
+	public static void main(String args[])
+	{
+		Scanner SC=new Scanner(System.in);
+		int num=SC.nextInt();
+        int count=0;
+        while(num!=0)
+        {
+            num/=10;
+                count++;
+
+        }
+            System.out.print(count);
+	}
+}

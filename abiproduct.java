4ae373316f3c4d8c5b5e331d55b5475188f672f5
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 11:50:16 +0530

    Create abiproduct.java

---
+++ abiproduct.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class prodectvalue
+{
+    public static void main(String args[]){
+        int m, n, sum = 1;
+        Scanner s = new Scanner(System.in);
+        m = s.nextInt();
+        while(m > 0){
+            n = m % 10;
+            sum = sum * n;
+            m = m / 10;
+        }
+        System.out.println(sum);
+    }
+}

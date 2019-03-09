d184f06ab9053e6c8271463bdbac69a122d6865a
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 11:48:31 +0530

    Create findvalueabi.java

---
+++ findvalueabi.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class findvalue
+{	
+	public static void main(String[] args){
+		int a,b,c;
+        Scanner	sc = new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+      
+        System.out.print((a*b)%c);
+	}
+}

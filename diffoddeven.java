02a35251678d3ce887c9a46f9a6948288447f3ff
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 10:06:55 +0530

    Create diffoddeven.java

---
+++ diffoddeven.java
@@ -0,0 +1,21 @@
+ import java.util.Scanner;
+ class diffevenoddnumbers
+{
+    public static void main(String args[])
+    {
+        
+        Scanner s = new Scanner(System.in);
+        int m = s.nextInt();
+        int n = s.nextInt();
+        int d=m-n;
+       
+            if(d%2==0)
+            {
+                System.out.println("even");
+            }
+            else
+            {
+                System.out.println("odd");
+            }
+    }
+}

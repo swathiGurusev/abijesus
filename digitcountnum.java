11092e52b59767af58d0d5b66187ce85846034fc
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 10:59:51 +0530

    Create digitcountnum.java

---
+++ digitcountnum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+ class digitcountnum 
+{
+    public static void main(String args[])
+    {
+    Scanner input =new Scanner(System.in);
+    int n1,n2,i; 
+	 n1=input.nextInt(); 
+        n2=input.nextInt();	 
+    if(n1>n2)
+	{ 
+        System.out.println(n1-n2);
+	}
+	else
+	{ 
+        
+        System.out.println(n2-n1);
+	}
+    }
+ 
+}

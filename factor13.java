6cff1a4457aa64a111e4bac765271e33c0fafbe0
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 10:48:49 +0530

    Create factor13.java

---
+++ factor13.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+class factors13numbers
+{
+   public static void main(String args[])
+   {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+      
+           if(num%13==0)
+           {
+               System.out.print("yes");
+           }
+           else
+           {
+                System.out.print("no");
+           }
+      

8b5d34e48f8672a31cd4df35e7041b12c3d7d438
swathiGurusev <47376275+swathiGurusev@users.noreply.github.com>
2019-03-09 13:25:04 +0530

    Create checkholiday.java

---
+++ checkholiday.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class checkholiday
+{
+    public static void main(String arg[]){
+        Scanner ip=new Scanner(System.in);
+        String day=ip.next().toLowerCase();        
+        if(day.equals("sunday")||day.equals("saturday"))
+        {
+            System.out.print("yes");    
+        }
+        else
+        {
+            System.out.print("no");
+        }
+    }
+}
